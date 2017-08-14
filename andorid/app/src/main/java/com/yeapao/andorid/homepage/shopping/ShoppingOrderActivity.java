0c21b228e3413fd0d6f9a267a6becbea63188fa3
fujindong <alanfujindong@gmail.com>
2017-08-14 16:51:26 +0800

    add new alipay

--- andorid/app/src/main/java/com/yeapao/andorid/homepage/shopping/ShoppingOrderActivity.java
+++ andorid/app/src/main/java/com/yeapao/andorid/homepage/shopping/ShoppingOrderActivity.java
@@ -1,23 +1,22 @@
 package com.yeapao.andorid.homepage.shopping;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.Keep;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.mikephil.charting.charts.PieRadarChartBase;
-import com.scottfu.sflibrary.alipay.AlipayManager;
-import com.scottfu.sflibrary.alipay.OrderCommitted;
+import com.alipay.sdk.app.PayTask;
+import com.scottfu.sflibrary.alipay.AuthResult;
 import com.scottfu.sflibrary.alipay.PayResult;
 import com.scottfu.sflibrary.util.LogUtil;
 import com.yeapao.andorid.R;
@@ -26,9 +25,7 @@ import com.yeapao.andorid.base.BaseActivity;
 import com.yeapao.andorid.model.CallPaymentModel;
 import com.yeapao.andorid.model.LessonOrderModel;
 
-import org.w3c.dom.Text;
-
-import java.security.PrivateKey;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,6 +55,9 @@ public class ShoppingOrderActivity extends BaseActivity {
 
     private LessonOrderModel lessonOrderModel;
     private String payMentType;
+
+    private static final int SDK_PAY_FLAG = 1;
+    private static final int SDK_AUTH_FLAG = 2;
 
 
     public static void start(Context context, String typeId, String price, String id) {
@@ -152,37 +152,89 @@ public class ShoppingOrderActivity extends BaseActivity {
                         LogUtil.e(TAG, model.getErrmsg());
                         if (model.getErrmsg().equals("ok")) {
 
-                            OrderCommitted result = new OrderCommitted();
-                            result.setOrderID(model.getData().getOrderCode());
-                            result.setOrderName("yepao");
-                            String mmm = model.getData().getPrice();
-                            result.setFinalPrice(mmm);
-                            result.setNameList("yepao");
-                            AlipayManager.payOrder(result,"http://47.92.113.97:8080/yepao/order/alipay",(Activity)getContext(),handler);
-//                            AlipayManager.payOrderV2(result,model.getData().getAliPayInfo(),(Activity)getContext(),handler);
+                            final String orderInfo = model.getData().getAliPayInfo();   // 订单信息
+
+                            Runnable payRunnable = new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    PayTask alipay = new PayTask(ShoppingOrderActivity.this);
+                                    Map<String, String> result = alipay.payV2(orderInfo,true);
+                                    Log.e("msp", result.toString());
+                                    Message msg = new Message();
+                                    msg.what = SDK_PAY_FLAG;
+                                    msg.obj = result;
+                                    mHandler.sendMessage(msg);
+                                }
+                            };
+                            // 必须异步调用
+                            Thread payThread = new Thread(payRunnable);
+                            payThread.start();
+
+
+//
+//                            OrderCommitted result = new OrderCommitted();
+//                            result.setOrderID(model.getData().getOrderCode());
+//                            result.setOrderName("yepao");
+//                            String mmm = model.getData().getPrice();
+//                            result.setFinalPrice(mmm);
+//                            result.setNameList("yepao");
+//                            AlipayManager.payOrder(result,"http://47.92.113.97:8080/yepao/order/alipay",(Activity)getContext(),handler);
+////                            AlipayManager.payOrderV2(result,model.getData().getAliPayInfo(),(Activity)getContext(),handler);
                         }
                     }
                 };
 
 
 
-
-    Handler handler = new Handler(){
-        public void handleMessage(Message msg){
-            PayResult payResult = new PayResult((String) msg.obj);
-            String resultInfo = payResult.getResult();
-            String resultStatus = payResult.getResultStatus();
-            if(TextUtils.equals(resultStatus, "9000")){
-                Toast.makeText(getContext(), "支付成功", Toast.LENGTH_SHORT).show();
-                ((Activity)getContext()).finish();
-            }else {
-                if(TextUtils.equals(resultStatus, "8000")){
-                    Toast.makeText(getContext(), "支付结果确认中", Toast.LENGTH_SHORT).show();
-                }else {
-                    Toast.makeText(getContext(), "支付失败", Toast.LENGTH_SHORT).show();
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SDK_PAY_FLAG: {
+                    @SuppressWarnings("unchecked")
+                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
+                    /**
+                     对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为9000则代表支付成功
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        Toast.makeText(getContext(), "支付成功", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
+                        Toast.makeText(getContext(), "支付失败", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
                 }
+                case SDK_AUTH_FLAG: {
+                    @SuppressWarnings("unchecked")
+                    AuthResult authResult = new AuthResult((Map<String, String>) msg.obj, true);
+                    String resultStatus = authResult.getResultStatus();
+
+                    // 判断resultStatus 为“9000”且result_code
+                    // 为“200”则代表授权成功，具体状态码代表含义可参考授权接口文档
+                    if (TextUtils.equals(resultStatus, "9000") && TextUtils.equals(authResult.getResultCode(), "200")) {
+                        // 获取alipay_open_id，调支付时作为参数extern_token 的value
+                        // 传入，则支付账户为该授权账户
+                        Toast.makeText(getContext(),
+                                "授权成功\n" + String.format("authCode:%s", authResult.getAuthCode()), Toast.LENGTH_SHORT)
+                                .show();
+                    } else {
+                        // 其他状态值则为授权失败
+                        Toast.makeText(getContext(),
+                                "授权失败" + String.format("authCode:%s", authResult.getAuthCode()), Toast.LENGTH_SHORT).show();
+
+                    }
+                    break;
+                }
+                default:
+                    break;
             }
-        }
+        };
     };
 
 
